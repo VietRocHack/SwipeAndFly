fb32f859e3eaaae3534a08e131eb65ebeeb05ea4
Vuong Ho <hochivuong2002@gmail.com>
2024-12-11 17:16:41 -0500

    Add static folder

--- backend/src/routes/frontend_routes.py
+++ backend/src/routes/frontend_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, send_from_directory
 
-frontend_bp = Blueprint("frontend", __name__)
+frontend_bp = Blueprint("frontend", __name__, static_folder="../../../frontend/dist")
 
 
 @frontend_bp.route('/')
 def index():
-    return send_from_directory("../frontend/dist", 'index.html')
+    return send_from_directory(frontend_bp.static_folder, 'index.html')
 
