129ecae629e65ba3452f41e28804ceacfc6d1d53
Vuong Ho <hochivuong2002@gmail.com>
2024-12-12 17:52:32 -0500

    Add groq into openai_request

--- backend/src/function/video_analysis/openai_request.py
+++ backend/src/function/video_analysis/openai_request.py
@@ -1,33 +1,21 @@
 import cv2
 import base64
-from dotenv import load_dotenv
 import os
 import json
 import io
-from src.function.video_analysis import utils
 import time
+
+from backend.src.function.video_analysis.shared import utils
 from aiohttp import ClientSession, ClientError
-from openai import OpenAI
+from backend.src.function.video_analysis.shared.const import version_configs
 
 logger = utils.setup_logger(__name__, f"./openai_request_logger_{int(time.time())}.log")
-
-load_dotenv()
-
-client = OpenAI(
-    base_url="https://api.groq.com/openai/v1",
-    api_key=os.environ.get("GROQ_API_KEY")
-)
-VISON_MODEL = "llama-3.2-11b-vision-preview"
-
-print(os.curdir)
-
-with open("./src/function/video_analysis/openai_analysis_json_template.txt") as f:
-	analysis_template = f.read()
 
 async def analyze_images(
 		session: ClientSession,
 		images: list,
-		metadata: dict[str, str] = {}
+		metadata: dict[str, str] = {},
+		version: str = "openai"
 	) -> dict:
 	"""
 		Receives a list of images supposedly to be sampled from a video, gives them
@@ -35,27 +23,28 @@ async def analyze_images(
 
 		Metadata is optional, and is provided as-is to the prompt to OpenAI
 	"""
+	
+	# Get version config based on recommendation model version
+	config = version_configs[version]
+ 
 	# Convert the images to JPG format
 	base_64_list = []
 	for image in images:
 		_, image_jpg = cv2.imencode('.jpg', image)
 		base_64_list.append(base64.b64encode(image_jpg.tobytes()).decode('utf-8'))
 
-	headers = {
-		"Content-Type": "application/json",
-		"Authorization": f"Bearer { os.environ.get('GROQ_API_KEY') }"
-	}
-
+	# Prepare content for messages
 	content = []
 	content.append(
 		{
 			"type": "text",
 			"text": """These images are from a TikTok video. """
 			"""Analyze this video using simple and to-the-point vocab using this json format: """
-			f"""{ analysis_template }"""
+			f"""{ config.analysis_template }"""
 			f"""Included is a metadata of the video for better analysis: {json.dumps(metadata)} """
 		})
-	
+
+	# Prepare images in user message
 	for base64_image in base_64_list:
 		content.append(
 			{
@@ -66,40 +55,33 @@ async def analyze_images(
 				}
 			})
 
-	# payload = {
-	# 	"model": "llama-3.2-11b-vision-preview",
-  	# "response_format": {"type": "json_object"},
-	# 	"messages": [
-	# 		{
-	# 			"role": "user",
-	# 			"content": content
-	# 		}
-	# 	],
-	# 	"max_tokens": 200
-	# }
-	# return await _send_request(session, payload, headers)
+	# Define headers
+	headers = {
+		"Content-Type": "application/json",
+		"Authorization": f"Bearer { config.api_key }"
+	}
 
-	print("Completing message")
-
-	output = client.chat.completions.create(
-		messages=[
+	# Define payload
+	payload = {
+		"model": config.model,
+		"response_format": {
+      		"type": "json_object"
+        },
+		"messages": [
 			{
 				"role": "user",
 				"content": content
 			}
 		],
-		model=VISON_MODEL,
-	)
-
-	analysis_raw = output.choices[0].message.content
-	print(analysis_raw)
-	analysis_json = json.loads(analysis_raw)
-	return analysis_json
+		"max_tokens": 200
+	}
+	return await _send_request(session, payload, headers)
 
 async def analyze_transcript(
 		session: ClientSession,
 		transcript: str,
-		metadata: dict[str, str] = {}
+		metadata: dict[str, str] = {},
+		version: str = "openai"
 	) -> dict:
 	"""
 		Receives a list of images supposedly to be sampled from a video, gives them
@@ -108,22 +90,28 @@ async def analyze_transcript(
 		Metadata is optional, and is provided as-is to the prompt to OpenAI
 	"""
 
-	headers = {
-		"Content-Type": "application/json",
-		"Authorization": f"Bearer { os.environ.get('GROQ_API_KEY') }"
-	}
+	# Get version config based on recommendation model version
+	config = version_configs[version]
 
+	# Prepare images in user message
 	content = []
 	content.append(
 		{
 			"type": "text",
 			"text": """This is a transcript from a TikTok video. """
 			"""Analyze this video in details using simple and to-the-point vocab using this json format: """
-			f"""{ analysis_template }"""
+			f"""{ config.analysis_template }"""
 			f"""Included is a metadata of the video for more things to analyze: {json.dumps(metadata)} """
 			f"""Transcript: { transcript }"""
 		})
 
+	# Define headers
+	headers = {
+		"Content-Type": "application/json",
+		"Authorization": f"Bearer { config.api_key }"
+	}
+
+	# Define payload
 	payload = {
 		"model": "gpt-4o-mini",
   	"response_format": {"type": "json_object"},
@@ -141,21 +129,22 @@ async def analyze_transcript(
 async def _send_request(
 		session: ClientSession,
 		payload: dict,
-		headers: dict
+		headers: dict,
+		config: dict,
 	) -> str:
 
-	logger.info(f"Sending request to OpenAI with payload { payload }")
+	logger.info(f"Sending request to LLM Provider with payload { payload }")
 
 	try:
 		payload_bytes = io.BytesIO(json.dumps(payload).encode('utf-8'))
 		async with session.post(
-			url="https://api.openai.com/v1/chat/completions",
+			url=config.base_url,
 			data=payload_bytes,
 			headers=headers
 		) as response:
 			response_json = await response.json()
 
-			logger.info(f"Reponse received from OpenAI with code {response.status}: {json.dumps(response_json)}")
+			logger.info(f"Reponse received from LLM Provider with code {response.status}: {json.dumps(response_json)}")
 
 			analysis_raw = response_json["choices"][0]["message"]["content"]
 
@@ -163,9 +152,9 @@ async def _send_request(
 			return analysis_json
 
 	except ClientError as e:
-		logger.error(f"ClientError during requesting OpenAI: {e}")
+		logger.error(f"ClientError during requesting LLM Provider: {e}")
 		return {"error": "An error has happened"}
 	
 	except Exception as e:
-		logger.error(f"Some happened during requesting OpenAI: {e}")
+		logger.error(f"Some happened during requesting LLM Provider: {e}")
 		return {"error": "An error has happened"}
